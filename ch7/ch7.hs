3d76d420e8e2df8adfdeb7d9e5c6c6464125f2e2
Tushar Tyagi <tushar4r@gmail.com>
2016-04-18 21:56:05 +0530

    Update

--- ch7/ch7.hs
+++ ch7/ch7.hs
@@ -13,5 +13,21 @@ any' f xs | length elements /= 0 = True
           | otherwise = False
           where elements = filter f xs
 
---takeWhile' :: (a -> Bool) -> [a] -> [a]
---takeWhile' f (x:xs) | f x == True 
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' f (x:xs) | f x == True = x : (takeWhile' f xs)
+                    | otherwise = []
+
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' f xs = takeWhile' (not . f) xs
+
+-- 3
+-- The parameters in lambda are the current value from the list and
+-- the accumulated value.
+-- Lambda has to return the new value of accumulator based on the
+-- current input.
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x y -> f x : y) [] xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p xs = foldr f [] xs
+  where f v acc = if p v then v : acc else acc
